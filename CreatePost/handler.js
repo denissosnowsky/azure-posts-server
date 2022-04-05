b31f73865807fbdcc7843e2550d38a83c1659708
denissosnowsky <denissosnowsky@gmail.com>
2022-04-05 18:55:02 +0300

    feat: added headers

--- CreatePost/handler.js
+++ CreatePost/handler.js
@@ -1,29 +1,36 @@
-const { insertEntity } = require('../services/tableService');
+const { insertEntity } = require('../services/tableService')
 
 const createPostHandler = async (context) => {
-    try {
-        const { blog, title, content } = context.req.body
+  try {
+    const { blog, title, content } = context.req.body
 
-        const entity = {
-            PartitionKey: { _: blog },
-            RowKey: { _: new Date().getTime().toString() },
-            title: { _: title },
-            content: { _: content },
-        }
-
-        const result = await insertEntity('Posts', entity)
-
-        context.res = {
-            body: result,
-        }
-        context.done();
-    } catch (error) {
-        context.res = {
-            status: 500,
-            body: error.message,
-        }
-        context.done();
+    const entity = {
+      PartitionKey: { _: blog },
+      RowKey: { _: new Date().getTime().toString() },
+      title: { _: title },
+      content: { _: content },
     }
+
+    const result = await insertEntity('Posts', entity)
+
+    context.res = {
+      body: result,
+    }
+    context.done()
+  } catch (error) {
+    context.res = {
+      status: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'X-Requested-With': '*',
+        'Access-Control-Allow-Headers':
+          'X-requested-with,Content-type,Accept,Origin,Authorization,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+      },
+      body: error.message,
+    }
+    context.done()
+  }
 }
 
-module.exports = createPostHandler;
+module.exports = createPostHandler
--- DeletePost/index.js
+++ DeletePost/index.js
@@ -13,6 +13,13 @@ module.exports = async function (context, req) {
   } catch (error) {
     context.res = {
       status: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'X-Requested-With': '*',
+        'Access-Control-Allow-Headers':
+          'X-requested-with,Content-type,Accept,Origin,Authorization,Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Allow-Methods',
+        'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
+      },
       body: error.message,
     }
   }
